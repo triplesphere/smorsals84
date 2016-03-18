0fd5a8f80aa2265b69446bc17da8e12d9adc03b8
Jiiks <jiiks@windowslive.com>
2016-03-18 04:08:41 +0200

    Make timestamp and dited_timestamp nullable

--- DiscordArchiver/data/DMessage.cs
+++ DiscordArchiver/data/DMessage.cs
@@ -6,10 +6,10 @@ namespace DiscordArchiver.data {
         public DAttachment[] attachments;
         public bool tts;
         public DEmbed[] embeds;
-        public DateTime timestamp;
+        public DateTime? timestamp;
         public bool mention_everyone;
         public string id;
-        public DateTime edited_timestamp;
+        public DateTime? edited_timestamp;
         public DAuthor author;
         public string content;
         public string channel_id;
